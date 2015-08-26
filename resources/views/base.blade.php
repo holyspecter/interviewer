7b73849f18216dbc817170a1ad9d2c4226a97124
r.kiuchko <r.kliuchko@levi9.com>
2015-08-26 12:04:57 +0300

    bootstrap added through CDN

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -3,7 +3,8 @@
     <head>
         <title>@yield('title')</title>
 
-        <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     </head>
     <body>
         <div class="container">
