37a5b891d70be9253b1a9f90bed5d7e922b7eba7
r.kiuchko <r.kliuchko@levi9.com>
2015-08-26 15:50:53 +0300

    route parameters' requirements added

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -21,14 +10,14 @@ Route::resource('companies', 'CompanyController');
 Route::get('companies/{companyId}/positions/create', [
     'as' => 'positions.create',
     'uses' => 'PositionController@create',
-]);
+])->where(['companyId' => '\d+']);
 
 Route::post('companies/{companyId}/positions', [
     'as' => 'positions.store',
     'uses' => 'PositionController@store',
-]);
+])->where(['companyId' => '\d+']);
 
 Route::get('positions/{id}', [
     'as' => 'positions.show',
     'uses' => 'PositionController@show',
-]);
+])->where(['id' => '\d+']);
