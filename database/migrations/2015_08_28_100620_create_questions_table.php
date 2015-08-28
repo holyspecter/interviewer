31fde1433803fbe90518ab66951025a720642a99
r.kiuchko <r.kliuchko@levi9.com>
2015-08-28 19:14:28 +0300

    tmp commit

---
+++ database/migrations/2015_08_28_100620_create_questions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('questionnaire_id');
+            $table->string('question');
+
+            $table->foreign('questionnaire_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('questions');
+    }
+}
